1ea066ca197bee4b09a4dafc0ec2d5c81030cc0f
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-10-14 09:29:51 +0200

    Add user to user messaging in same server

--- app/chat-service/internal/handler/message_handler.go
+++ app/chat-service/internal/handler/message_handler.go
@@ -1,22 +1,28 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/nsmsb/darda-chat/app/chat-service/internal/model"
 )
 
+type Client struct {
+	List map[*websocket.Conn]bool
+}
+
 type MessageHandler struct {
-	connections map[*websocket.Conn]bool
+	connections map[string]*Client
 	mutex       sync.Mutex
 }
 
 func NewMessageHandler() *MessageHandler {
 	return &MessageHandler{
-		connections: make(map[*websocket.Conn]bool),
+		connections: make(map[string]*Client),
 	}
 }
 
@@ -27,42 +33,74 @@ var upgrader = websocket.Upgrader{
 }
 
 func (handler *MessageHandler) HandleConnections(c *gin.Context) {
+	// Get id of user and handler error
+	userId := c.Query("id")
+	if userId == "" {
+		fmt.Println("User ID is required")
+		c.JSON(http.StatusBadRequest, gin.H{
+			"erro": "User ID is required",
+		})
+		return
+	}
+	fmt.Printf("User %s connected\n", userId)
+
 	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		fmt.Println("upgrade error:", err)
 		return
 	}
+
 	// Ensure cleanup when the function exits
 	defer func() {
 		ws.Close()
 		handler.mutex.Lock()
-		delete(handler.connections, ws)
+		connList, ok := handler.connections[userId]
+		if !ok {
+			delete(connList.List, ws)
+		}
 		handler.mutex.Unlock()
 		fmt.Println("client disconnected and removed")
 	}()
 
 	// Add connection
 	handler.mutex.Lock()
-	handler.connections[ws] = true
+	connections, ok := handler.connections[userId]
+	if !ok {
+		connections = &Client{List: make(map[*websocket.Conn]bool)}
+		handler.connections[userId] = connections
+	}
+	connections.List[ws] = true
 	handler.mutex.Unlock()
 
 	for {
-		_, msg, err := ws.ReadMessage()
+		_, raw, err := ws.ReadMessage()
 		if err != nil {
 			fmt.Println("read error:", err)
-			handler.mutex.Lock()
-			delete(handler.connections, ws)
-			handler.mutex.Unlock()
 			break
 		}
 
-		// Broadcast message to all connected clients
+		var msg model.Message
+		if err := json.Unmarshal(raw, &msg); err != nil {
+			fmt.Println("unmarshal error:", err)
+			continue
+		}
+		fmt.Println("new msg", msg)
+
+		// TODO: Validation
+
+		// Prepare message to send
+		broadcastData := fmt.Sprintf(`{"sender":"%s","content":"%s"}`, msg.Sender, msg.Content)
+
+		// Broadcast message to all connected clients of destination
 		handler.mutex.Lock()
-		for client := range handler.connections {
-			if err := client.WriteMessage(websocket.TextMessage, msg); err != nil {
-				fmt.Println("broadcast error:", err)
-				client.Close()
-				delete(handler.connections, client)
+		if destConnections, ok := handler.connections[msg.Destination]; ok {
+			// Sending to all destination connections
+			for client := range destConnections.List {
+				if err := client.WriteMessage(websocket.TextMessage, []byte(broadcastData)); err != nil {
+					fmt.Println("broadcast error:", err)
+					client.Close()
+					delete(destConnections.List, client)
+				}
 			}
 		}
 		handler.mutex.Unlock()

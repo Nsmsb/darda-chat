098aa23a8e9535fe574501ad4a4fb0532f4ebc33
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-10-29 16:56:09 +0100

    Added default fields to logger

--- app/chat-service/internal/middleware/logger.go
+++ app/chat-service/internal/middleware/logger.go
@@ -11,14 +11,27 @@ import (
 func ZapLogger(logger *zap.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
-
-		c.Next() // process request
-
-		latency := time.Since(start)
-		statusCode := c.Writer.Status()
 		clientIP := c.ClientIP()
+		requestId := c.GetString(RequestIDKey)
 		method := c.Request.Method
 		path := c.Request.URL.Path
+
+		// Create a logger with request-specific fields
+		reqLogger := logger.With(
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("request_id", requestId),
+			zap.String("client_ip", clientIP),
+		)
+		// Store the logger in the context for use in handlers
+		c.Set("logger", reqLogger)
+
+		// Process request
+		c.Next()
+
+		// Log request details
+		latency := time.Since(start)
+		statusCode := c.Writer.Status()
 
 		logger.Info("incoming request",
 			zap.Int("status", statusCode),

f678c2c21b4f9473efc297e7e22ad5f50b93b407
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-11-02 14:52:14 +0100

    spell fix

--- app/message-writer-service/internal/handler/message_handler.go
+++ app/message-writer-service/internal/handler/message_handler.go
@@ -9,7 +9,7 @@ import (
 type MessageHandler struct {
 	dbName         string
 	collectionName string
-	dbCliebnt      *mongo.Client
+	dbClient       *mongo.Client
 }
 
 // NewMessageHandler creates a new MessageHandler instance.
@@ -17,13 +17,13 @@ func NewMessageHandler(dbName, collectionName string) *MessageHandler {
 	return &MessageHandler{
 		dbName:         dbName,
 		collectionName: collectionName,
-		dbCliebnt:      db.Client(),
+		dbClient:       db.Client(),
 	}
 }
 
 // Handle handles the message and writes it to the database
 func (h *MessageHandler) Handle(msg model.Message) error {
-	collection := h.dbCliebnt.Database(h.dbName).Collection(h.collectionName)
+	collection := h.dbClient.Database(h.dbName).Collection(h.collectionName)
 	_, err := collection.InsertOne(nil, msg)
 	return err
 }

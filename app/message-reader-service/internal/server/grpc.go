24573baf004253a670931f8d54adf8cb0e4c58b6
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-11-16 00:47:15 +0100

    Added GRPC server init with interceptors and services

---
+++ app/message-reader-service/internal/server/grpc.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	pb "github.com/nsmsb/darda-chat/app/message-reader-service/internal/api/message/gen"
+	"github.com/nsmsb/darda-chat/app/message-reader-service/internal/server/interceptor"
+	"github.com/nsmsb/darda-chat/app/message-reader-service/internal/service"
+	"github.com/nsmsb/darda-chat/app/message-reader-service/pkg/logger"
+	"google.golang.org/grpc"
+)
+
+func NewMessageGRPCServer() *grpc.Server {
+	logger := logger.Get()
+	defer logger.Sync()
+
+	// Create server and add interceptors
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(
+			interceptor.ChainUnaryInterceptors(
+				interceptor.UnaryRecoveryInterceptor(logger),
+				interceptor.UnaryZapInterceptor(logger),
+			),
+		),
+		grpc.StreamInterceptor(
+			interceptor.ChainStreamInterceptors(
+				interceptor.StreamRecoveryInterceptor(logger),
+				interceptor.StreamZapInterceptor(logger),
+			),
+		),
+	)
+
+	// Creating message service
+	messageService := service.NewMessageService()
+
+	// Register Message service
+	pb.RegisterMessageServiceServer(server, messageService)
+
+	return server
+}

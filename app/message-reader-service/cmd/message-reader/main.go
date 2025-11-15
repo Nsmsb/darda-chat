a094e32abbc4408bcc9108e03fa9f80723daece8
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-11-16 00:49:39 +0100

    Added main with graceful shutdown

---
+++ app/message-reader-service/cmd/message-reader/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/nsmsb/darda-chat/app/message-reader-service/internal/config"
+	"github.com/nsmsb/darda-chat/app/message-reader-service/internal/server"
+	"github.com/nsmsb/darda-chat/app/message-reader-service/pkg/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	// Get configuration
+	config := config.Get()
+
+	// Getting logger
+	logger := logger.Get()
+	defer logger.Sync()
+
+	// Create listener
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", config.Port))
+	if err != nil {
+		logger.Fatal("failed to listen", zap.Error(err))
+	}
+
+	// Create gRPC server with already registered handlers
+	s := server.NewMessageGRPCServer()
+
+	go func() {
+
+		logger.Info("server listening on port", zap.String("port", config.Port))
+		if err := s.Serve(lis); err != nil {
+			panic(err)
+		}
+	}()
+
+	// graceful shutdown
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	s.GracefulStop()
+	fmt.Println("server stopped")
+}

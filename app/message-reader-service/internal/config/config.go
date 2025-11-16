9d0f74950baa842da79820c6ac4506e729158e7b
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-11-16 13:02:58 +0100

    Added MessagePageSize config

--- app/message-reader-service/internal/config/config.go
+++ app/message-reader-service/internal/config/config.go
@@ -12,6 +12,7 @@ import (
 // Config holds the configuration values for the application.
 type Config struct {
 	Port                string
+	MessagePageSize     int
 	MongoDBName         string
 	MongoCollectionName string
 	MongoAddr           string
@@ -30,16 +31,21 @@ var (
 
 // Get returns the singleton instance of Config, it reads the configs only once.
 func Get() *Config {
-	var redisDB int = 0 // default value
+	var messagesPageSize, redisDB int = 20, 0 // default value
 	var err error
 	redisDB, err = strconv.Atoi(getEnv("REDIS_DB", "0"))
 	if err != nil {
 		logger.Get().Error("Invalid REDIS_DB, using default", zap.Int("value", redisDB), zap.Error(err))
 	}
+	messagesPageSize, err = strconv.Atoi(getEnv("MESSAGE_PAGE_SIZE", "20"))
+	if err != nil {
+		logger.Get().Error("Invalid MESSAGE_PAGE_SIZE, using default", zap.Int("value", messagesPageSize), zap.Error(err))
+	}
 
 	once.Do(func() {
 		instance = &Config{
 			Port:                getEnv("PORT", "50051"),
+			MessagePageSize:     messagesPageSize,
 			MongoDBName:         getEnv("MONGO_DB_NAME", "darda_chat"),
 			MongoCollectionName: getEnv("MONGO_COLLECTION_NAME", "messages"),
 			MongoAddr:           getEnv("MONGO_ADDR", "mongodb://localhost:27017"),

8f83708515f571b1e8ae91aa163c6fece74c5b2e
Nassim SBAIHIA <nassim.sbaihia@gmail.com>
2025-11-23 18:44:39 +0100

    Add outbox message

--- app/message-writer-service/internal/model/message.go
+++ app/message-writer-service/internal/model/message.go
@@ -11,3 +11,10 @@ type Message struct {
 	Content        string    `json:"content" bson:"content"`
 	Timestamp      time.Time `json:"timestamp" bson:"timestamp"`
 }
+
+type OutboxMessage struct {
+	ID          string `json:"id" bson:"_id"`
+	Payload     Message
+	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
+	ProcessedAt time.Time `json:"processed_at" bson:"processed_at"`
+}
